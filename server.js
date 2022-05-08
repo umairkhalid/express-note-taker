3cbe3cf7af01876fc6d1c1385925eec8628d9f80
umairkhalid <umairkhalid@fastmail.fm>
2022-05-08 21:23:20 +1000

    routes are estabilished in server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require('path');
 const fs = require('fs');
 const util = require('util');
 const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
+const { v4: uuidv4 } = require('uuid');
+const res = require('express/lib/response');
 
 const PORT = process.env.PORT || 3001;
 
@@ -14,6 +16,42 @@ app.use(express.urlencoded({ extended: true }));
 //api.use('/api', api);
 
 app.use(express.static('public'));
+
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// GET Route for retrieving all the tips
+app.get('/api/notes', (req, res) => {
+    console.info(`${req.method} request received for notes`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a note`);
+    
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+
+            title,
+            text,
+            noteId: uuidv4(),
+        
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added successfully`);
+    } else {
+        res.error('Error in adding note');
+    }
+});
+
+// Wildcard route to direct users to a index.html page
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
